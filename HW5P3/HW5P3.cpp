90e6ba936dd47a03b0ac3f57f14bb1b3c4b8f2c8
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-09-22 15:29:09 +0000

    royal mess of for loops and if statements, but functioning iteration.

--- HW5P3/HW5P3.cpp
+++ HW5P3/HW5P3.cpp
@@ -5,17 +5,29 @@ using namespace std;
 
 int main() {
 
-  int numbers[10];
+  int numbers[10]; // list of values
+  int a = 0;       // for us in for loop
 
   // inputs values to list
   for (int i = 10 - 1; i >= 0; i--) {
     cin >> numbers[i];
   }
 
-  
-  for (int i = 10 - 1; i >= 0; i--)
-    cout << numbers[i];
-  // if numbers
-  //[i] == numbers[0] {}
+  for (int i = 0; i <= 9; i++) {
+    if (numbers[i] == numbers[a]) {
+      //[i] == numbers[0]
+    } else {
+      for (int a = 0; a <= 9; a++)
+        cout << numbers[i] << " " << numbers[a] << endl;
+      if (numbers[i] == numbers[a]) {
+        copy(numbers + 3,  // copy everything starting here
+             numbers + 9,  // and ending here, not including it,
+             numbers + 2); // to this destination
+        cout << "New list is: ";
+        for (int c = 0; c <= 9; c++)
+          cout << numbers[c];
+      }
+    }
+  }
   return 0;
 }
