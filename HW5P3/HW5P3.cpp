6f50a2056f677d564612e519d557e1efe94a62a5
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-09-22 15:47:12 +0000

    HW5P3 simplified code. Need to debug why unique number of modulo int is returning negative value.

--- HW5P3/HW5P3.cpp
+++ HW5P3/HW5P3.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main() {
 
-  int numbers[10]; // list of values
-  int a = 0;       // for us in for loop
+  int numbers[10];  // list of input values
+  int modulo[10];   // modulo list
+  int divisor = 42; // modulo divisor
+  int num_of_modulo = 10;
 
   // inputs values to list
   for (int i = 10 - 1; i >= 0; i--) {
@@ -14,20 +16,19 @@ int main() {
   }
 
   for (int i = 0; i <= 9; i++) {
-    if (numbers[i] == numbers[a]) {
-      //[i] == numbers[0]
-    } else {
-      for (int a = 0; a <= 9; a++)
-        cout << numbers[i] << " " << numbers[a] << endl;
-      if (numbers[i] == numbers[a]) {
-        copy(numbers + 3,  // copy everything starting here
-             numbers + 9,  // and ending here, not including it,
-             numbers + 2); // to this destination
-        cout << "New list is: ";
-        for (int c = 0; c <= 9; c++)
-          cout << numbers[c];
+    modulo[i] = (numbers[i] % divisor);
+  }
+
+  for (int i = 0; i <= 9; i++) {
+    for (int a = i + 1; a <= 9; a++) {
+      if (modulo[i] == modulo[a]) {
+        copy(modulo + a,  // copy everything starting here
+             modulo + 9,  // and ending here, not including it,
+             modulo + i); // to this destination
+        num_of_modulo--;
       }
     }
   }
+  cout << num_of_modulo;
   return 0;
 }
